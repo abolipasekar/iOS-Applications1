eb3efc80cc0f82a8ec32fa6355103e237ba7f490
Aboli <aboli@abolis-macbook-air.local>
2016-12-08 13:22:38 +0530

    Simple Calculator Application

--- SimpleCalculator/ViewController.h
+++ SimpleCalculator/ViewController.h
@@ -9,6 +9,25 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    BOOL operatorpressed;
+    BOOL add;
+}
+
+//@property (nonatomic) BOOL *operatorpressed;
+//@property(nonatomic)BOOL *add;
+@property(nonatomic,retain) NSString *firstentry;
+@property(nonatomic,retain) NSString *secondentry, *tag1;
+
+
+@property (weak, nonatomic) IBOutlet UILabel *labeloutput;
+
+- (IBAction)Clear:(id)sender;
+- (IBAction)Add:(id)sender;
+- (IBAction)Minus:(id)sender;
+- (IBAction)Equal:(id)sender;
+-(IBAction)number:(UIButton *)sender;
+
 
 
 @end
--- SimpleCalculator/ViewController.m
+++ SimpleCalculator/ViewController.m
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    operatorpressed=false;
+    _firstentry=NULL;
+    _secondentry=NULL;
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -23,5 +27,88 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)Clear:(id)sender {
+    
+    _labeloutput.text=NULL;
+    _firstentry=NULL;
+    _secondentry=NULL;
+    operatorpressed=NULL;
+    
+}
+
+- (IBAction)Add:(id)sender {
+    
+    add=TRUE;
+    operatorpressed=TRUE;
+}
+
+- (IBAction)Minus:(id)sender {
+    
+    add=false;
+    operatorpressed=true;
+}
+
+- (IBAction)Equal:(id)sender {
+    
+    if(add==false)
+    {
+        int outputnum=[_firstentry intValue]-[_secondentry intValue];
+        _labeloutput.text=[NSString stringWithFormat:@"%i",outputnum];
+        
+    }
+    else{
+        int outputnum=[_firstentry intValue]+[_secondentry intValue];
+        _labeloutput.text=[NSString stringWithFormat:@"%i",outputnum];
+    }
+    
+    operatorpressed=false;
+    _firstentry=NULL;
+    _secondentry=NULL;
+    
+    
+    
+}
+
+-(IBAction)number:(UIButton *)sender
+{
+     int tag=sender.tag;
+    if(operatorpressed==FALSE)
+    {
+        if(_firstentry==NULL)
+        {
+            _firstentry=[NSString stringWithFormat:@"%d",tag];
+            _labeloutput.text=_firstentry;
+        }
+        
+        else
+        {
+            _firstentry=[NSString stringWithFormat:@"%@", _firstentry];
+            _labeloutput.text=_firstentry;
+        }
+        
+    }
+    else{
+        if(_secondentry==NULL)
+        {
+            _secondentry=[NSString stringWithFormat:@"%d",tag];
+            _labeloutput.text=_secondentry;
+
+        }
+        else{
+          _secondentry=[NSString stringWithFormat:@"%@", _secondentry];
+            _labeloutput.text=_secondentry;
+
+        }
+    }
+    
+    
+    
+}
+
+
+
+
+
 
 @end
