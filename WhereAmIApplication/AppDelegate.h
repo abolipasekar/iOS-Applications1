1efe74c4a0c8e84a263e26ef464c0be6fb746315
Aboli <aboli@abolis-macbook-air.local>
2016-12-12 18:07:59 +0530

    Initial Commit

---
+++ WhereAmIApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhereAmIApplication
+//
+//  Created by Aboli on 12/12/16.
+//  Copyright © 2016 Aboli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhereAmIApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhereAmIApplication
+//
+//  Created by Aboli on 12/12/16.
+//  Copyright © 2016 Aboli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
